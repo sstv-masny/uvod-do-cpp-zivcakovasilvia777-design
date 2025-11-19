aca00c7b5b3615a3d748e351bea188c4529d8063
L-kolencik <zivcakovasilvia777@gmail.com>
2025-11-19 13:27:40 +0100

    Update main.cpp

--- task_08_frequency_table/main.cpp
+++ task_08_frequency_table/main.cpp
@@ -1,11 +1,9 @@
 
 #include <iostream>
+#include <string>
 #include <vector>
 int main() {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    int n; 
-    if (!(std::cin >> n)) return 0;
-    // TODO: read n values in [0,100], count, and print only those with count>0 in ascending value
+    int number[5] = {1, 2, 3, 4, 5};
+    std::cout << number[2];
     return 0;
 }
