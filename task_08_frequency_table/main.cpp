f35d5912af312c76693737a1ab471073b429b2b7
L-kolencik <zivcakovasilvia777@gmail.com>
2025-11-19 14:05:16 +0100

    Update main.cpp
    neviem

--- task_08_frequency_table/main.cpp
+++ task_08_frequency_table/main.cpp
@@ -1,9 +1,24 @@
 
 #include <iostream>
-#include <string>
 #include <vector>
+
+using namespace std;
+
 int main() {
-    int number[5] = {1, 2, 3, 4, 5};
-    std::cout << number[2];
+    int n[101] = {};
+
+    n[1] += 1;
+    n[2] += 1;
+    n[2] += 1;
+    n[100] += 1;
+    n[0] += 1;
+
+    for (int hodnota = 0; hodnota < 101; hodnota++) {
+
+        if (n[hodnota] > 0) {
+            cout << hodnota << " " << n[hodnota] << "\n";
+        }
+    }
+
     return 0;
 }
