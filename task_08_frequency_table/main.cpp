7c5884bfb422c82b8c241da635d83adfd62350e5
zivcakovasilvia777-design <zivcakovasilvia777@gmail.com>
2025-12-16 08:27:48 +0100

    Update main.cpp

--- task_08_frequency_table/main.cpp
+++ task_08_frequency_table/main.cpp
@@ -5,20 +5,11 @@
 using namespace std;
 
 int main() {
-    int n[101] = {};
-
-    n[1] += 1;
-    n[2] += 1;
-    n[2] += 1;
-    n[100] += 1;
-    n[0] += 1;
-
-    for (int hodnota = 0; hodnota < 101; hodnota++) {
-
-        if (n[hodnota] > 0) {
-            cout << hodnota << " " << n[hodnota] << "\n";
-        }
-    }
-
+std:
+    ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    int n;
+    if (!std::cin >> n)
+        return 0;
     return 0;
 }
