12da3fd831aba6ea77aa0a67427848206b8c476a
zivcakovasilvia777-design <zivcakovasilvia777@gmail.com>
2025-12-16 08:17:42 +0100

    .
    .

--- task_05_sum_1_to_n/main.cpp
+++ task_05_sum_1_to_n/main.cpp
@@ -1,20 +1,4 @@
 
-
-#include <iostream>
-int main() {
-    long long n;
-    if (!(std::cin >> n))
-        return 0;
-    __int128 m = 0;
-    long long i = 1;
-    while (i <= n) {
-        m += i;
-        ++i;
-    }
-    long long out = static_cast<long long>(acc);
-    std::cout << out << '\n';
-    return 0;
-}
 #include <iostream>
 int main() {
     long long n;
@@ -29,3 +13,19 @@ int main() {
     std::cout << out << '\n';
     return 0;
 }
+
+// #include <iostream>
+// int main() {
+// long long n;
+// if (!(std::cin >> n))
+// return 0;
+//__int128 m = 0;
+// long long i = 1;
+// while (i <= n) {
+//  m += i;
+//++i;
+//}
+// long long out = static_cast<long long>(acc);
+// std::cout << out << '\n';
+// return 0;
+//}
